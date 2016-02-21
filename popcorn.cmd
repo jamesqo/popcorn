16a7e9e6eba613e5bda47538fb52fee1cf9b2a14
James Ko <jamesqko@gmail.com>
2016-02-21 16:27:05 -0500

    Add the logic for the Windows script

--- popcorn.cmd
+++ popcorn.cmd
@@ -1,2 +1,56 @@
 @echo off
 setlocal
+
+set "output=nul"
+set "times=1"
+
+:help
+
+:parseArgs
+
+if "%~1" == "" goto main
+
+if /i "%~1" == "-?" goto help
+if /i "%~1" == "-h" goto help
+if /i "%~1" == "--help" goto help
+
+if /i "%~1" == "-t" (
+    set "times=%~2"
+    shift
+)
+
+if /i "%~1" == "--times" (
+    set "times=%~2"
+    shift
+)
+
+if /i "%~1" == "-o" (
+    set "output=%~2"
+    shift
+)
+
+if /i "%~1" == "--output" (
+    set "output=%~2"
+    shift
+)
+
+shift
+
+:curl
+
+@powershell -NoProfile "iwr %~1 -OutFile %~2" 2> nul
+
+goto :eof
+
+:downloadPackage
+
+set "url=https://www.nuget.org/api/v2/package/%~1"
+call :curl "%url%" %2
+
+goto :eof
+
+:main
+
+for /l %%i in (1, 1, %times%) do (
+    call :downloadPackage "%package%" "%output%"
+)
